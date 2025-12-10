36ba944ef8e58cbf3ad507541c7a3a313836bc84
cagr1 <carlos.gallardo@espoch.edu.ec>
2025-12-10 12:26:05 -0500

    Add Analitycs2

--- src/main.js
+++ src/main.js
@@ -2,5 +2,7 @@ import { createApp } from 'vue'
 import './main.css'
 import App from './App.vue'
 import { i18n } from './i18n'
+import { inject} from '@vercel/analytics'
 
+inject()
 createApp(App).use(i18n).mount('#app')
